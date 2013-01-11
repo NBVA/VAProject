c48dd82d92685866499aab01f992665981870344
jing92 <jing92@host32-197.wifi.ubc.ca>
2013-01-10 16:37:32 -0800

    test5

--- src/test/Test.java
+++ src/test/Test.java
@@ -2,9 +2,16 @@ package test;
 
 import java.util.ArrayList;
 
-public class Test {
+public  class Test {
 	//comments
 	
 	ArrayList i = [];
+	
 	//sadl;kfjaslkfjas
+	
+	public int fun(){
+		return 0;
+	}
 }
+
+
